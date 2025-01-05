eb0f2b36e80c4067f7ad32c450950d0e0a30c926
feb11fff <136089445+feb11fff@users.noreply.github.com>
2025-01-05 12:11:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -121,16 +121,12 @@ with st.container():
                 from selenium.webdriver.chrome.options import Options
                 from selenium.common.exceptions import NoSuchElementException, WebDriverException
                 import os
-                CHROMEDRIVER_PATH = os.environ.get('CHROMEDRIVER_PATH', '/usr/local/bin/chromedriver')
-                GOOGLE_CHROME_BIN = os.environ.get('GOOGLE_CHROME_BIN', '/usr/bin/google-chrome')
-                # Konfigurasi opsi headless
-                options = Options()
-                options.binary_location = GOOGLE_CHROME_BIN
-                options.add_argument('--disable-gpu')
-                options.add_argument('--no-sandbox')
-                options.headless = True
-                
-                driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,options=options)
+                chrome_options = webdriver.ChromeOptions()
+                chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+                chrome_options.add_argument("--headless")
+                chrome_options.add_argument("--disable-dev-shm-usage")
+                chrome_options.add_argument("--no-sandbox")
+                driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
                 # URL dari Google Search
                 url='https://www.google.com/maps/place/Jaddih+Hill+Madura/@-7.0822777,112.7569647,17z/data=!4m8!3m7!1s0x2dd8045eb0acb79d:0x4a24af02fd796f55!8m2!3d-7.082283!4d112.7595396!9m1!1b1!16s%2Fg%2F11c2r8kctr?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D'
                 driver.get(url)
