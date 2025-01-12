b764c60d4fd405c07726692903f3a06bcac61b5d
feb11fff <136089445+feb11fff@users.noreply.github.com>
2025-01-12 22:18:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -226,6 +226,7 @@ with st.container():
                         # Buat dataframe
                 flat_data = [item for sublist in id_ulasan for item in sublist]
                 flat_datawaktu = [item for sublist in tanggal_ulasan for item in sublist]
+                flat_datawaktu.pop(0)
                 datas = {'id_review': flat_data, 'Review': review_texts, 'waktu': flat_datawaktu}
                 data_scrapping = pd.DataFrame(datas)
                 data_scrapping = data_scrapping.drop_duplicates(subset='id_review')
