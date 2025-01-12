b78bd66c9011581612ee33a0b0d442efe5f3f9c9
feb11fff <136089445+feb11fff@users.noreply.github.com>
2025-01-13 04:14:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -159,10 +159,10 @@ with st.container():
                 # URL dari Google Search
                 url= 'https://www.google.com/maps/place/Pantai+Slopeng/@-6.8861093,113.7820433,15z/data=!4m8!3m7!1s0x2dd9ea23fabac2df:0x8550176773c06614!8m2!3d-6.8861095!4d113.792343!9m1!1b1!16s%2Fg%2F112yfwt6c?entry=ttu&g_ep=EgoyMDI1MDEwOC4wIKXMDSoASAFQAw%3D%3D&hl=id&gl=ID'
                 driver.get(url)
-                time.sleep(5)
+                time.sleep(10)
                 tombol = driver.find_element(By.XPATH, "//*[@id='QA0Szd']/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[7]/div[2]/button")
                 tombol.click()
-                time.sleep(5)
+                time.sleep(10)
                 # Klik menggunakan JavaScript
                 terbaru_button = driver.find_element(By.XPATH, "//div[@class='mLuXec'][contains(text(),'Terbaru')]")
                 driver.execute_script("arguments[0].click();", terbaru_button)
