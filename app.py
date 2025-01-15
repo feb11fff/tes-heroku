699273c9887bc7d3d3545e60cc25f179efc0002c
feb11fff <136089445+feb11fff@users.noreply.github.com>
2025-01-16 01:30:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -708,12 +708,12 @@ with st.container():
                 # Buat dataframe
                 data = {'id_review': flat_data, 'Review': review_texts, 'waktu': combined_list}
                 df = pd.DataFrame(data)
-                if time_range=='Minggu':
-                    df = df[~df['waktu'].str.contains('bulan', case=False, na=False)]
-                    df = df[~df['waktu'].isin([batasoutput2,batasoutput3,batasoutput4])]
-                else :
-                    df = df[~df['waktu'].str.contains('tahun', case=False, na=False)]
-                    df = df[~df['waktu'].isin([batasoutput2,batasoutput3,batasoutput4])]
+                # if time_range=='Minggu':
+                #     df = df[~df['waktu'].str.contains('bulan', case=False, na=False)]
+                #     df = df[~df['waktu'].isin([batasoutput2,batasoutput3,batasoutput4])]
+                # else :
+                #     df = df[~df['waktu'].str.contains('tahun', case=False, na=False)]
+                #     df = df[~df['waktu'].isin([batasoutput2,batasoutput3,batasoutput4])]
                 df
 
             except FileNotFoundError:
